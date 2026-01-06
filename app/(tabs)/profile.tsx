690f9faf0e68a28f1233c3b63e79b87eec6b71a9
BrunoSilva <bmfas.2002@gmail.com>
2026-01-06 17:11:21 +0000

    Admin Push

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,18 @@
 // app/(tabs)/profile.tsx
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function ProfileScreen() {
+  const router = useRouter();
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Profile Screen</Text>
+
+      <Button
+        title="Ir para Admin"
+        onPress={() => router.push('/admin')}
+      />
     </View>
   );
 }
